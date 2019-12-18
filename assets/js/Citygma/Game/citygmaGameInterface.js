2b686ec7bbfe1636b3ae99ac57f7ffa978ef956c
dlaxcess <flipiste@free.fr>
2019-12-19 00:49:23 +0100

    test lockorientation2

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -70,13 +70,27 @@ export default class CitygmaGameInterface extends Component {
         this.activateCompass = this.activateCompass.bind(this);
     }
 
+    fullScreen() {
+        // Kind of painful, but this is how it works for now
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+    }
+
     componentWillUnmount() {
         this.props.toggleHeader(true);
     }
 
 
     componentDidMount() {
-
+        this.fullScreen();
+        screen.orientation.lock("portrait");
 
 
 
