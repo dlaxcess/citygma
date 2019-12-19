63c83bdc46282793a92db69305467c480ab2b17f
dlaxcess <flipiste@free.fr>
2019-12-19 21:42:28 +0100

    test video safari3

--- assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
+++ assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
@@ -15,7 +15,7 @@ export default class VideoPlayerComponent extends Component {
             controls: false,
             light: false,
             volume: 0.8,
-            muted: false,
+            muted: true,
             played: 0,
             loaded: 0,
             duration: 0,
