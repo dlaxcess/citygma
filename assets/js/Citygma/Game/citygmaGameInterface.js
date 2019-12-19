361cd769a4b3cc5bf19e63ad8449626a9f2b9f9c
dlaxcess <flipiste@free.fr>
2019-12-19 05:32:36 +0100

    test nouveau geoloc14

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -465,7 +465,7 @@ export default class CitygmaGameInterface extends Component {
 
         return (
             <Fragment>
-                <div id="GameInterfaceGenContainer">
+                <div id="GameInterfaceGenContainer" onClick={this.fullScreen}>
                     <div id="compass" className={this.state.userDeviceAcceptCompass && this.state.showCompass && this.state.currentEnigmaActiveCompass ? 'compassVisible' : 'compassHidden'}>
                         <div id="arrow"><img src={logo}/></div>
                         <div id="notice"></div>
