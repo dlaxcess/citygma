c3e8da2fa10444d0ca89e5728c3c67b987b1f83c
dlaxcess <flipiste@free.fr>
2019-12-19 04:34:05 +0100

    test nouveau geoloc6

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -255,7 +255,7 @@ export default class CitygmaGameInterface extends Component {
     // Compass
     activateCompass() {
 
-        function error(err) {
+        /*function error(err) {
             console.warn('ERROR(' + err.code + '): ' + err.message);
         }
 
@@ -268,14 +268,14 @@ export default class CitygmaGameInterface extends Component {
             maximumAge: 0
         };
 
-        this.setState({watchPositionId: navigator.geolocation.watchPosition(this.bearingListener, error, options)});
+        this.setState({watchPositionId: navigator.geolocation.watchPosition(this.bearingListener, error, options)});*/
 
-        /*if (window.DeviceOrientationEvent) {
+        if (window.DeviceOrientationEvent) {
             //document.getElementById("notice").innerHTML = "super ça marche.";
             window.addEventListener('deviceorientation', this.bearingListener, false);
         } else {
             document.getElementById("notice").innerHTML = "Helaas. De DeviceOrientationEvent API word niet door dit toestel ondersteund.";
-        }*/
+        }
     }
 
     deviceOrientationHandler(tiltLR, tiltFB, fromNorthBearing, bearedDir) {
