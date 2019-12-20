b18402471448b549a882db0095bd7f60ac655ebe
dlaxcess <flipiste@free.fr>
2019-12-20 03:19:28 +0100

    test double systeme compass31

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -518,10 +518,10 @@ export default class CitygmaGameInterface extends Component {
         let coords = eventData.coords;
         //let dir = coords.heading;
 
-        let bearedDir = 0;
+        //let bearedDir = 0;
 
         let fromNorthBearing = this.getBearing(coords.latitude, coords.longitude, this.state.currentLat, this.state.currentLong);
-        bearedDir = this.wrap360(this.getBearing(coords.latitude, coords.longitude, this.state.currentLat, this.state.currentLong) - coords.heading);
+        let bearedDir = this.wrap360(this.getBearing(coords.latitude, coords.longitude, this.state.currentLat, this.state.currentLong) - coords.heading);
 
         this.deviceOrientationHandler(tiltLR, coords.heading, fromNorthBearing, bearedDir);
     }
