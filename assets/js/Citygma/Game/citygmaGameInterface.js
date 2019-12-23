fdad7501c968d1099952ae63ad34caad648d379f
dlaxcess <flipiste@free.fr>
2019-12-23 13:14:54 +0100

    test lockscreen css

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -764,9 +764,9 @@ export default class CitygmaGameInterface extends Component {
 
         return (
             <Fragment>
-                {/*
+                
                 <div id="landscapePleaseChangeOrientation"><h2>S&lsquo;il vous plais, pour le bon fonctionnement du jeu, veuillez passez votre écran en mode portrait :)</h2></div>
-                */}
+
                 <div id="GameInterfaceGenContainer" onClick={this.fullScreen}>
                     { this.state.showEnterGameScreen &&
                     <div id="enterGameScreen">
