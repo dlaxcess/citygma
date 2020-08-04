7e3b07e1a318e4e935b63310efa815b871f271bf
dlaxcess <flipiste@free.fr>
2020-08-04 19:09:55 +0200

    test highcurrancy true

--- assets/js/Citygma/Game/geoloc/usePosition.js
+++ assets/js/Citygma/Game/geoloc/usePosition.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 
 const defaultSettings = {
-    enableHighAccuracy: false,
+    enableHighAccuracy: true/*false*/,
     timeout: Infinity,
     maximumAge: 0,
 };
