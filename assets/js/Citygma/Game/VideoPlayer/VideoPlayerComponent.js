84009b2b7e07963ad46553758de9c6002116af05
dlaxcess <flipiste@free.fr>
2020-08-12 14:15:43 +0200

    test video mutee puis non avec nouveau settimeout2

--- assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
+++ assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
@@ -176,14 +176,14 @@ export default class VideoPlayerComponent extends Component {
     render() {
         const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state;
 
-        var timeOutID = window.setTimeout( function () {
-            /*if (true) {
-                var evt = document.createEvent("MouseEvents");
-                evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
-                document.getElementById("videoRePlay").dispatchEvent(evt);
-            }*/
+        /*var timeOutID = window.setTimeout( function () {
+            //if (true) {
+                //var evt = document.createEvent("MouseEvents");
+                //evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                //document.getElementById("videoRePlay").dispatchEvent(evt);
+            //}
             this.setMuted(false);
-        }, 1000);
+        }, 1000);*/
 
         return (
             /*setTimeout(this.setMuted(false), 1000);*/
