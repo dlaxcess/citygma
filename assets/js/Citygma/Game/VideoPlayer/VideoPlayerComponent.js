7420b30e04fb54636b460939ba724c7b87518462
dlaxcess <flipiste@free.fr>
2020-08-12 14:08:08 +0200

    test video mutee puis non avec nouveau settimeout

--- assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
+++ assets/js/Citygma/Game/VideoPlayer/VideoPlayerComponent.js
@@ -20,7 +20,7 @@ export default class VideoPlayerComponent extends Component {
             controls: false,
             light: false,
             volume: 1,
-            muted: false,
+            muted: true,
             played: 0,
             loaded: 0,
             duration: 0,
@@ -58,18 +58,18 @@ export default class VideoPlayerComponent extends Component {
         */}
         this.player.subscribeToStateChange(this.handleStateChange.bind(this));
 
-        var timeOutID = window.setTimeout( function () {
+    }
+
+    componentDidUpdate() {
+        this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+
+        /*var timeOutID = window.setTimeout( function () {
             if (!this.state.showEndedButton) {
                 var evt = document.createEvent("MouseEvents");
                 evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
                 document.getElementById("videoRePlay").dispatchEvent(evt);
             }
-        }, 5000);
-
-    }
-
-    componentDidUpdate() {
-        this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+        }, 5000);*/
     }
 
     setMuted(muted) {
@@ -176,13 +176,14 @@ export default class VideoPlayerComponent extends Component {
     render() {
         const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state;
 
-        /*var timeOutID = window.setTimeout( function () {
-            if (true) {
+        var timeOutID = window.setTimeout( function () {
+            /*if (true) {
                 var evt = document.createEvent("MouseEvents");
                 evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
                 document.getElementById("videoRePlay").dispatchEvent(evt);
-            }
-        }, 5000);*/
+            }*/
+            this.setMuted(false);
+        }, 1000);
 
         return (
             /*setTimeout(this.setMuted(false), 1000);*/
