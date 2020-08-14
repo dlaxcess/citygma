cb75bd76a92e8e513c5c21ffc43f6377f7cbfe99
dlaxcess <flipiste@free.fr>
2020-08-14 11:24:02 +0200

    test video lancee en permanence et relancee38

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -299,12 +299,12 @@ export default class CitygmaGameInterface extends Component {
                         const enigmaKey = Math.round(this.state.userAdvance) - 1;
 
                         if (this.state.enigmas[enigmaKey]) {
-                            this.setState({videoUrl: this.state.enigmas[enigmaKey].enigmaVideoIntroClue});
+                            this.setState({videoPlayerKey: this.state.userAdvance + 0.1, videoUrl: this.state.enigmas[enigmaKey].enigmaVideoIntroClue});
                         } else {
                             this.setState({videoUrl: this.state.adventure.videoLastEnigmaFilename});
                         }
 
-                        this.setState({userAdvance: this.state.userAdvance + 0.1, videoPlayerKey: this.state.userAdvance + 0.1, videoPlaying: true, displayVideo: false, showEnigma: true, geolocateShow: false, showCompass: false, showEnterGameScreen: false});
+                        this.setState({userAdvance: this.state.userAdvance + 0.1, videoPlaying: true, displayVideo: false, showEnigma: true, geolocateShow: false, showCompass: false, showEnterGameScreen: false});
 
 
 
@@ -359,7 +359,7 @@ export default class CitygmaGameInterface extends Component {
                 // Retour enigme FINALE
                 }else if (Math.round((this.state.userAdvance % 0.5)*100)/100 ===0.2) {
                     alert('useradvance: ' + this.state.userAdvance);
-                    this.setState({showEnigma: false, videoPlayerKey: this.state.userAdvance, videoPlaying: true, displayVideo: true, geolocateShow: false, showCompass: false, userAdvance: Math.round(this.state.userAdvance), showEnterGameScreen: false});
+                    this.setState({showEnigma: false, /*videoPlayerKey: this.state.userAdvance, */videoPlaying: true, displayVideo: true, geolocateShow: false, showCompass: false, userAdvance: Math.round(this.state.userAdvance), showEnterGameScreen: false});
                     this.storeUserAdvance(Math.round(this.state.userAdvance));
                 }
             }
