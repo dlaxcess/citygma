0c2626adc729c2da46e3aac3d709056297073a2d
dlaxcess <flipiste@free.fr>
2020-08-14 16:49:29 +0200

    test video lancee en permanence et relancee52

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -364,7 +364,7 @@ export default class CitygmaGameInterface extends Component {
                     this.storeUserAdvance(Math.round(this.state.userAdvance));
                 // Retour enigme FINALE
                 }else if (Math.round((this.state.userAdvance % 0.5)*100)/100 ===0.2) {
-                    alert('useradvance: ' + this.state.userAdvance);
+                    //alert('useradvance: ' + this.state.userAdvance);
                     this.setState({showEnigma: false, videoPlayerKey: this.state.userAdvance, videoPlaying: true, displayVideo: true, geolocateShow: false, showCompass: false, userAdvance: Math.round(this.state.userAdvance), showEnterGameScreen: false});
                     this.storeUserAdvance(Math.round(this.state.userAdvance));
                 }
