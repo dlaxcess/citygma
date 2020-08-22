66ddbf54f3b933b928e1f20639026101b25f98d9
dlaxcess <flipiste@free.fr>
2020-08-22 22:32:25 +0200

    test correction nosleep reactualisation page 3

--- assets/js/Citygma/Game/citygmaGameInterface.js
+++ assets/js/Citygma/Game/citygmaGameInterface.js
@@ -244,7 +244,7 @@ export default class CitygmaGameInterface extends Component {
 
     enableNoSleep() {
         //var noSleep = new NoSleep();
-        if (this.noSleep) this.noSleep.disable(); // Just to be sure if you forgot to disable.
+        //if (this.noSleep) this.noSleep.disable(); // Just to be sure if you forgot to disable.
         this.noSleep = new NoSleep();
 
 
